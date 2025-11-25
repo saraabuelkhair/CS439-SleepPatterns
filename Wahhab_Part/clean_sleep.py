2647613937fcd93d41ce92b864157f1037e598ac
Wahhab Ahmed <wa150@scarletmail.rutgers.edu>
2025-11-25 00:32:57 -0500

    Update clean_sleep.py

--- Wahhab_Part/clean_sleep.py
+++ Wahhab_Part/clean_sleep.py
@@ -1,1 +1,8 @@
+"""
+This file loads and cleans the Sleep Health dataset.
+
+Run in terminal using: "py clean_sleep.py"
+
+Load this cleaned dataset into MySQL using "db_utils.write_sleep_table(clean_sleep_dataset(...))"
+"""
 
