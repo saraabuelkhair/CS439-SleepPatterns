6a600aef0e87b18cebf72aa21b7fe6e6db77f832
Wahhab Ahmed <wa150@scarletmail.rutgers.edu>
2025-11-25 22:43:02 -0500

    Update db_utils.py

--- Wahhab_Part/db_utils.py
+++ Wahhab_Part/db_utils.py
@@ -1,1 +1,14 @@
+"""
+Database utility functions for the project.
 
+This file handles all interactions with MySQL, including:
+    - Connecting to the database
+    - Writing the cleaned Student dataset to MySQL
+    - Writing the cleaned Sleep dataset to MySQL
+    - Creating indexes for faster queries
+    - Creating the unified SQL view that combines both datasets
+
+You do NOT run this file directly.
+
+Make sure config.py is also updated to run this program
+"""
